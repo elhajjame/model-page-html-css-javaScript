dcc303c0371239815db5e64204baddade9a1358d
mehdi el-hajjame <el-hajjame@outlook.com>
2025-12-06 23:56:18 +0100

    working on small page for understanding DOM in javaScript

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+const model = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+const btnsOpenModel = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModel);
+
+for(let i = 0; i <btnsOpenModel.length; i++){
+
+    btnsOpenModel[i].addEventListener('click', function(){
+        console.log('button clicked');
+        model.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    })
+}
