f824b3d355c34e04df2d7627ef475842bfb857be
mehdi el-hajjame <el-hajjame@outlook.com>
2025-12-07 20:32:16 +0100

    working on a modal window understanding DOM concept

--- script.js
+++ script.js
@@ -4,13 +4,38 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModel = document.querySelector('.close-modal');
 const btnsOpenModel = document.querySelectorAll('.show-modal');
 
+const closeModel = function(){
+    overlay.classList.add('hidden');
+    model.classList.add('hidden')
+}
+
+const openModel = function(){
+    model.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
 console.log(btnsOpenModel);
 
-for(let i = 0; i <btnsOpenModel.length; i++){
-
-    btnsOpenModel[i].addEventListener('click', function(){
-        console.log('button clicked');
-        model.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    })
+for(let i =0; i < btnsOpenModel.length; i++){
+    btnsOpenModel[i].addEventListener('click', openModel);
 }
+
+btnCloseModel.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel)
+
+// for (let i = 0; i < btnsOpenModel.length; i++) {
+
+//     btnsOpenModel[i].addEventListener('click', function () {
+//         console.log('button clicked');
+//         model.classList.remove('hidden');
+//         overlay.classList.remove('hidden');
+//     })
+// }
+
+
+   
+
+
+// overlay.addEventListener('click', function () {
+//     model.classList.add('hidden');
+//     overlay.classList.add('hidden');
+// });
